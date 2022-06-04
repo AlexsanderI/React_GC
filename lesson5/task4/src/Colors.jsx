e09ed1ace07a4fe2ec3ce0f44937c87b48c3e540
Your Name <y.y.oleksander@gmail.com>
2022-06-05 02:03:44 +0300

    Done

--- lesson5/task4/src/Colors.jsx
+++ lesson5/task4/src/Colors.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
 
-const Colors = () => {
-  return <div>Hello, React!</div>;
-};
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+class Colors extends React.Component {
+  setBodyColor = (color, e) => {
+    console.log(e);
+    document.body.style.backgroundColor = color;
+  };
+
+  render() {
+    return (
+      <div className="colors">
+        <button
+          style={{ backgroundColor: RED }}
+          onClick={this.setBodyColor.bind(this, RED)}
+          className="colors__button"
+        />
+        <button
+          style={{ backgroundColor: GREEN }}
+          onClick={() => this.setBodyColor(GREEN)}
+          className="colors__button"
+        />
+        <button
+          style={{ backgroundColor: BLUE }}
+          onClick={e => this.setBodyColor(BLUE, e)}
+          className="colors__button"
+        />
+      </div>
+    );
+  }
+}
 
 export default Colors;
