eecb1d758cd27194598cf2203d39f841b93c906b
Your Name <y.y.oleksander@gmail.com>
2022-06-05 22:48:05 +0300

    1

--- lesson5/task5/src/Toggler.jsx
+++ lesson5/task5/src/Toggler.jsx
@@ -1,5 +1,5 @@
 // make button and check workin with class
-// meke click on button tern text 'On/Off'
+// meke click on button tern status 'On/Off'
 
 import React from 'react';
 
@@ -7,20 +7,27 @@ class Toggler extends React.Component {
   constructor(proper) {
     super(proper);
     this.state = {
-      counter: 'Off',
+      statusBtn: false,
     };
   }
 
-  reset = () => {
-    this.setState({
-      counter: 'On',
-    });
+  handClick = () => {
+    this.setState(state => ({
+      statusBtn: !state.statusBtn,
+    }));
   };
 
   render() {
+    if (this.state.statusBtn) {
+      return (
+        <button onClick={this.handClick} className="toggler">
+          Off
+        </button>
+      );
+    }
     return (
-      <button onClick={this.reset} className="toggler">
-        {this.state.counter}
+      <button onClick={this.handClick} className="toggler">
+        On
       </button>
     );
   }
