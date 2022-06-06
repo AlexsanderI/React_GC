9ba563958d611388f4394fcb2693d9d875994faa
Your Name <y.y.oleksander@gmail.com>
2022-06-07 01:32:09 +0300

    Done

---
+++ lesson6/task2/src/Mailbox.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Mailbox = ({ unreadMesasges }) => (
+  <div className="mailbox">
+    <span className="mailbox__text">Mesasges</span>
+    {unreadMesasges.length > 0 && <span className="mailbox__count">{unreadMesasges.length}</span>}
+  </div>
+);
+
+export default Mailbox;
