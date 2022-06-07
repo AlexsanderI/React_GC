42058e49ddddd599c7d6382b9a4eca174c55ea6e
Your Name <y.y.oleksander@gmail.com>
2022-06-07 13:07:59 +0300

    2

--- lesson6/task2/src/index.jsx
+++ lesson6/task2/src/index.jsx
@@ -5,4 +5,4 @@ import Mailbox from './Mailbox.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<Mailbox unreadMesasges={['4']} />, rootElement);
+ReactDOM.render(<Mailbox unreadMesasges={[]} />, rootElement);
