7380dc49b0a466fbdeb591e452d4d67408b31e6f
Your Name <y.y.oleksander@gmail.com>
2022-06-07 13:01:30 +0300

    1

--- lesson6/task2/src/Mailbox.jsx
+++ lesson6/task2/src/Mailbox.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Mailbox = ({ unreadMesasges }) => (
   <div className="mailbox">
-    <span className="mailbox__text">Mesasges</span>
+    <span className="mailbox__text">Messages</span>
     {unreadMesasges.length > 0 && <span className="mailbox__count">{unreadMesasges.length}</span>}
   </div>
 );
