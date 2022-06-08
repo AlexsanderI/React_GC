fb332ab4402b7bb04273f824741eb0061137bfc0
Your Name <y.y.oleksander@gmail.com>
2022-06-08 23:55:14 +0300

    1

--- lesson07/task3/src/Transaction.jsx
+++ lesson07/task3/src/Transaction.jsx
@@ -4,18 +4,18 @@ import moment from 'moment';
 const formatDate = date => moment(date).format('DD MMM');
 const formatTime = date => moment(date).format('hh:mm');
 
-// const Transaction = ({ time, date, from, to, rate, amount }) => (
-//   <li className="transaction">
-//     <span className="transaction__date">{formatDate(time)}</span>
-//     {/* <span className="transaction__time">{formatTime(date)}</span> */}
-//     <span className="transaction__assets">
-//       {from} → {to}
-//     </span>
-//     <span className="transaction__rate">{rate}</span>
-//     <span className="transaction__amount">
-//       {new Intl.NumberFormat('en-GB', { maximumSignificantDigits: 3 }).format(amount)}
-//     </span>
-//   </li>
-// );
+const Transaction = ({ from, to, rate, amount }) => (
+  <li className="transaction">
+    {/* <span className="transaction__date">{formatDate(time)}</span> */}
+    {/* <span className="transaction__time">{formatTime(date)}</span> */}
+    <span className="transaction__assets">
+      {from} → {to}
+    </span>
+    <span className="transaction__rate">{rate}</span>
+    <span className="transaction__amount">
+      {new Intl.NumberFormat('en-GB', { maximumSignificantDigits: 3 }).format(amount)}
+    </span>
+  </li>
+);
 
 export default Transaction;
