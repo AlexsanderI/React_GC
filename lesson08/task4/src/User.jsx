54708c1a16c83eafef69e080bed42ba306028e4d
Your Name <y.y.oleksander@gmail.com>
2022-06-10 22:09:03 +0300

    Подгрузка данных с сервера

---
+++ lesson08/task4/src/User.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    user: null,
+  };
+
+  componentDidMount() {
+    this.fetchUserId(this.props.userId);
+  }
+
+  fetchUserId = userId => {
+    fetch(`https://api.github.com/users/${userId}`)
+      .then(respons => respons.json())
+      .then(data => {
+        this.setState({
+          user: data,
+        });
+      });
+  };
+
+  render() {
+    const { user } = this.state;
+    if (!user) {
+      return null;
+    }
+    const { avatar_url, location, name } = user;
+
+    return (
+      <div className="user">
+        <img alt="User Avatar" src={avatar_url} className="user__avatar" />
+        <div className="user__info">
+          <span className="user__name">{name}</span>
+
+          <span className="user__location">{location}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
