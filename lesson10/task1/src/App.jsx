68498fe2ee0d1baea19328978ccbbe9997993129
Your Name <y.y.oleksander@gmail.com>
2022-06-16 01:31:13 +0300

    State lifting

--- lesson10/task1/src/App.jsx
+++ lesson10/task1/src/App.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import UserProfile from './UserProfile';
+import UserMenu from './UserMenu';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+class App extends React.Component {
+  state = {
+    userData: null,
+  };
 
+  componentDidMount() {
+    this.fetchUserData(this.props.userId);
+  }
+
+  fetchUserData = userId => {
+    const userUrl = `https://api.github.com/users/${userId}`;
+    fetch(userUrl)
+      .then(respons => respons.json())
+      .then(userData =>
+        this.setState({
+          userData,
+        }),
+      );
+  };
+
+  render() {
+    return (
+      <div className="page">
+        <header className="header">
+          <UserMenu userData={this.state.userData} />
+        </header>
+        <UserProfile userData={this.state.userData} />
+      </div>
+    );
+  }
+}
 export default App;
--- lesson10/task1/src/index.jsx
+++ lesson10/task1/src/index.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App.jsx';
+// import UserProfile from './UserProfile';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App userId="github" />, rootElement);
