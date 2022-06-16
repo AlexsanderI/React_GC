173566db292b8b1810013637b2ff2bb4d6d3d27c
Your Name <y.y.oleksander@gmail.com>
2022-06-16 20:10:30 +0300

    Список товаров

--- lesson10/task4/src/index.jsx
+++ lesson10/task4/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import FilterableProductTable from './App.jsx';
+import FilterableProductTable from './FilterableProductTable.jsx';
 
 const rootElement = document.querySelector('#root');
 
