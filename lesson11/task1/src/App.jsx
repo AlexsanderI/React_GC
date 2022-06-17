95f20291108a35adbcf95bff42fc89e64b4ba801
Your Name <y.y.oleksander@gmail.com>
2022-06-17 19:32:58 +0300

    Обновление компоненты

--- lesson11/task1/src/App.jsx
+++ lesson11/task1/src/App.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import Numbers from './Numbers';
+import EvenNumbers from './EvenNumbers';
+import OddNumbers from './OddNumbers';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+class App extends React.Component {
+  state = {
+    number: 0,
+  };
 
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState({
+        number: this.state.number + 1,
+      });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <OddNumbers title="Old numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={this.state.number} />
+        <EvenNumbers title="Even numbers" number={this.state.number} />
+        <Numbers title="All numbers" number={17} />
+      </div>
+    );
+  }
+}
 export default App;
---
+++ lesson11/task1/src/Numbers.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Numbers extends React.PureComponent {
+  render() {
+    return (
+      <div className="number">
+        <span className="number__title">{this.props.title}</span>
+        <span className="number__value">{this.props.number}</span>
+      </div>
+    );
+  }
+}
+export default Numbers;
