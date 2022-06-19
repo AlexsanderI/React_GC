4d3dd2ddce06f79f4749955b03e4002be4c87f83
Your Name <y.y.oleksander@gmail.com>
2022-06-20 02:17:47 +0300

    Expanded Component

--- lesson11/task3/src/App.jsx
+++ lesson11/task3/src/App.jsx
@@ -1,7 +1,56 @@
-import React from 'react';
+// make statick page
+// hide expand__contetnt
+// show expand__contetnt
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+import React from 'react';
+import Expand from './Expand';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true,
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      visible: !this.state.visible,
+    });
+  }
+
+  render() {
+    const { visible } = this.state;
+
+    return (
+      <div className="app">
+        <div className="expand border">
+          <div className="expand__header">
+            <span className="expand__title">Some title</span>
+            <button className="expand__toggle-btn" onClick={this.toggle}>
+              {visible ? '⬆️' : '⬇️'}
+            </button>
+          </div>
+          {this.state.visible && <Expand></Expand>}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default App;
+
+// render() {
+//   return (
+//     <div className="app">
+//       <button className="btn" onClick={this.showDialog}>
+//         Show
+//       </button>
+//       <Dialog isOpen={this.state.isOpen} onClose={this.hideDialog} title="some titel">
+//         <p>Some text</p>
+//       </Dialog>
+//     </div>
+//   );
+// }
+// export default App;
---
+++ lesson11/task3/src/Expand.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+const Expand = () => (
+  //   if (!isOpen) {
+  //     return null;
+  //   }
+  <div className="expand__content">
+    <p>
+      Hooks are a new addition in React 16.8. They let you use state and other React features
+      without writing a className.
+    </p>
+  </div>
+);
+export default Expand;
+
+// {
+/* <div className="dialog">
+      <div className="dialog__heading">
+        <div className="dialog__title">{title}</div>
+        <button className="dialog__close-btn" onClick={onClose}>
+          +
+        </button>
+      </div>
+      <div className="dialog__content"> {children}</div>
+    </div> */
+// }
