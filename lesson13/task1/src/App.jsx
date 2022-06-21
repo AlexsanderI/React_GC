0d24934169458cde1fb7c7559ecc45d262f4a08c
Your Name <y.y.oleksander@gmail.com>
2022-06-21 21:08:43 +0300

    React Router intro

--- lesson13/task1/src/App.jsx
+++ lesson13/task1/src/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Contacts from './Contacts';
+import PageNotFound from './PageNotFound';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+const App = () => (
+  <div className="page">
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Products">
+          <Products />
+        </Route>
+        <Route path="/Contacts">
+          <Contacts />
+        </Route>
+        <Route path="/*">
+          <PageNotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
--- lesson13/task2/src/App.jsx
+++ lesson13/task2/src/App.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Home';
+import Products from './Products';
+import Contacts from './Contacts';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+const App = () => (
+  <div className="page">
+    <BrowserRouter>
+      <Switch>
+        <Route>
+          <Home />
+        </Route>
+        <Route>
+          <Products />
+        </Route>
+        <Route>
+          <Contacts />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
