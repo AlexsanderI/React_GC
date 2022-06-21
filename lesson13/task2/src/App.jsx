36e2d41ce90046651100ed452348e02a2a561b19
Your Name <y.y.oleksander@gmail.com>
2022-06-22 02:29:19 +0300

    React Router - Вложенные роуты

--- lesson13/task2/src/App.jsx
+++ lesson13/task2/src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import Home from './Home';
 import Products from './Products';
-import Contacts from './Contacts';
 
 const App = () => (
   <div className="page">
     <BrowserRouter>
-      <Switch>
-        <Route>
-          <Home />
-        </Route>
-        <Route>
-          <Products />
-        </Route>
-        <Route>
-          <Contacts />
-        </Route>
-      </Switch>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/products" component={Products} />
     </BrowserRouter>
   </div>
 );
--- lesson13/task2/src/Home.jsx
+++ lesson13/task2/src/Home.jsx
@@ -4,9 +4,7 @@ function Home() {
   return (
     <div className="page__content">
       <h1>🏠</h1>
-      <a href="/products">To products</a>
     </div>
   );
 }
-
 export default Home;
